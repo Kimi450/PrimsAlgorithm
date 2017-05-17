0f8119fba70d176ddc9d5fee65fde631941b01a2
Kimi450 <karan_samani@yahoo.co.in>
2017-05-17 22:02:58 +0530

    rewrote the code for the algorithm with a new approach, where instead of being at a single node and going back, you are the tree and itterate through all the elements of the tree to get the cheapest extension to an element. Renamed the functions to more suitable names like 'start' and 'prim'. Commented out the debug console.log's. Most likely, this will be the final version of the code.

--- prim.js
+++ prim.js
@@ -7,17 +7,16 @@
     var primElements=[]
     var elementSize=4;
     var maxCost=100;
-    var elements=4;
+    var delay=200;
+    var elements=100;
     var small_1=[null,null,maxCost+1];
-    var small_2=[null,null,small_1];
     var index;
     var time= new Date().getTime();
-    var test=[]
     var costs=[]
     var pointer;
     var cost=0;
-    var count=0;
-    var done=new Set();
+    var done=[0];
+    var pusher
 
     document.addEventListener("DOMContentLoaded",init,false)
     function init(){
@@ -25,122 +24,84 @@
         context=canvas.getContext("2d");
         width = canvas.width;
         height = canvas.height;
-        interval=window.setInterval(prim, 1000);
+        interval=window.setInterval(start, 1);
     }
-    function prim(){
+    function start(){
         if (primStart===true){
-            done.add(0)
             //first iteration this will create the dots
             primStart=false;
+            // primElements=[{x:100,y:50},{x:200,y:50},{x:200,y:150},{x:100,y:150}]
+
             for(var i=0;i<elements;i++){
                 e={
                     x:getRandomNumber(0,width-elementSize),
                     y:getRandomNumber(0,height-elementSize)
-                }
-                costs.push([])
+                };
+                costs.push([]);
                 for(var j=0;j<elements;j++){
-                    costs[i].push(0)
+                    costs[i].push(0);
                 }
-                primElements.push(e);
-                test.push(e.cost)
+                 primElements.push(e);
                 context.fillRect(primElements[i].x,primElements[i].y,elementSize,elementSize);
             }
-
             //drawing lines between all the dots
             for (var i=0;i<primElements.length;i++){
                 for(var j=i+1; j<primElements.length;j++){
 
-
-                    /*
                     //add costs for the line part 1 (random cost for new lines)
-                    costs[i][j]=getRandomNumber(1,maxCost)
-                    */
-                    //set values for now
-                    
-
-
+                    costs[i][j]=getRandomNumber(1,maxCost);
 
                     //draw lines
                     draw(primElements[i].x,primElements[i].y,primElements[j].x,primElements[j].y);
                 }
                 //add costs to the lines part 2 (existing lines given the correct costs)
-                /*
-                pointer=0
+                pointer=0;
                 do {
-                    costs[i][pointer]=costs[pointer][i]
+                    costs[i][pointer]=costs[pointer][i];
                     pointer++;
                 }
                 while(pointer<i);
-                */
             }
-            costs=[[0,5,1,2],[5,0,3,2],[1,3,0,3],[2,2,3,0]]
+            // costs=[[0,5,1,2],[5,0,3,2],[1,3,0,3],[2,2,3,0]]
             context.strokeStyle = '#ffffff';
         }
         
-       console.log(costs[0],costs[1],costs[2],costs[3],cost)
+        //console.log(costs[0],costs[1],costs[2],costs[3],done)
         //code for prim
-        smallest()
+        prim();
     }
-    function smallest(){
-        var i=0;
-        //i=starting element
+    function prim(){
+
+        //i=starting elementS
         //j=destination element
-        do{
-            for (var j=i+1;j<primElements.length;j++){
-                console.log(cost+costs[i][j],cost)
-                if(costs[i][j]<=small_1[2] && !(done.has(j))){
-                    small_1=[i,j,costs[i][j]]
-                    console.log(1)
-                }
-            }cost+=small_1[2];
-            for( var j=i+1;j<primElements.length;j++){
-                if (cost+costs[i][j]<cost){
-                    
+        var currTime= new Date().getTime()
+        if(currTime>=time+delay){
+            pusher=false;
+            for(var i=0;i<done.length;i++){
+                // console.log(i)
+                for (var j=0;j<primElements.length;j++){//was j=done[i]+1 and that broke it
+                    if(costs[done[i]][j]<=small_1[2] && !(done.indexOf(j)>-1)){
+                        // console.log("if ",costs[done[i]][j],small_1[2])
+                        small_1=[done[i],j,costs[done[i]][j]];
+                        pusher=true;
+                    }
                 }
             }
-            done.add(small_1[1]);
-
-            context.strokeStyle = '#ffffff';
-            draw(primElements[i].x,primElements[i].y,primElements[small_1[1]].x,primElements[small_1[1]].y);
-            i=small_1[1]
-            small_1=small_2
-            // i=small_1[1];
+            // console.log("after if",pusher,small_1)
+            cost+=small_1[2];
+            if (pusher){
+                done.push(small_1[1]);
+                context.strokeStyle = '#ffffff';
+                // console.log(small_1+"--------")
+                draw(primElements[small_1[0]].x,primElements[small_1[0]].y,primElements[small_1[1]].x,primElements[small_1[1]].y);
+                draw(primElements[small_1[0]].x,primElements[small_1[0]].y,primElements[small_1[1]].x,primElements[small_1[1]].y);
+                small_1=[null,null,maxCost+1];
+            }
+            time=new Date().getTime()
         }
-        while(done.size<primElements.length && i<=0)
-        // console.log(done)
-
-
-
-
-
-
-
-
-
-        // var currTime=new Date().getTime();
-        // var i=0;
-        
-        // do{
-        //     if (currTime>=time+2000){      
-        //         // for(var i=0;i<primElements.length;i++){        
-        //             for(var j=i+1;j<primElements.length;j++){
-        //                 if(costs[i][j]<small_1[2]){
-        //                     // small_2=small_1;
-        //                     small_1=[i,j,costs[i][j]];//stored the element,line,cost
-                            
-        //                 }
-        //             }
-        //         // }
-        //         cost+=small_1[2];
-        //         count++;
-        //         i++;
-        //         time=new Date().getTime();
-        //         draw(primElements[i].x,primElements[i].y,primElements[j-1].x,primElements[j-1].y)
-        //     }
-        // }
-        // while(count<primElements.length);
-
-        
+        if (done.length===primElements.length){
+            stop();
+        }        
     }
     
     function stop(){
